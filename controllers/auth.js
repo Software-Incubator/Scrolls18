c8f7693eeb86c039825ed2e596a96f3fe4a06e98
suyashsrv7 <suyashsrv7@gmail.com>
2018-09-13 12:10:03 +0530

    Team id changed to numberic type

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,7 +31,7 @@ module.exports = {
             if(doc.length == 0){
                 const teamId = randomString.generate({
                     length: 5,
-                    charset: 'alphanumeric'
+                    charset: 'numeric'
                 });
                 teamId = "SC18" + teamId;
                 bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
